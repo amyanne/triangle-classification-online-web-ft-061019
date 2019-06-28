b1ad7aba077419e09c051afc8589163b05216ad1
Amy Westcott <amy.westcott@yahoo.com>
2019-06-28 18:18:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
   attr_accessor :side_1, :side_2, :side_3 
-  attr_reader :equilateral 
+  attr_reader :equilateral, :isosceles
   
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
@@ -12,6 +12,9 @@ class Triangle
     if side_1 == side_2 && side_2 == side_3
       :equilateral
     end 
+  else if side_1 != side_2 && side_2 == side_3
+    :isosceles
+    
   end 
   
 end
